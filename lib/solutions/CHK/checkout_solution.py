899d66bdc45117b7f60ce440306e7282da4a900e
sbx_user1051 <sbx_user1051@169.254.10.97>
2023-11-07 02:53:56 +0000

    Tue Nov 07 02:53:56 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,155 +1,111 @@
-
+from constants import ITEM_TABLE
+from offer_service import * 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-ITEM_TABLE = {
-            "A": {
+ITEM_TABLE_OLD = {"A": {
                     "price": 50,
+                    "offer_type": "PRICE_REDUCT_MULTI",
                     "offer_price": 130,
                     "offer_quantity": 3,
+                    "offer_type_1": "PRICE_REDUCT_MULTI",
                     "offer_price_1": 200,
                     "offer_quantity_1": 5
                     },
-            "B": {
+              "B": {
                     "price": 30,
+                    "offer_type": "PRICE_REDUCT_SINGLE",
                     "offer_price": 45,
                     "offer_quantity": 2
                     },
-            "C": {
+              "C": {
                     "price": 20,
+                    "offer_type": "",
                     "offer_price": 0,
                     "offer_quantity": 0
                     },
-            "D": {
+              "D": {
                     "price": 15,
+                    "offer_type": "",
                     "offer_price": 0,
                     "offer_quantity": 0
                     },
             "E": {
                     "price": 40,
+                    "offer_type": "BOGOO",
+                    "bogoo_item": "B",
                     "offer_price": 0,
                     "offer_quantity": 2
                     },
             "F": {
                     "price": 10,
+                    "offer_type": "BOGO",
                     "offer_price": 0,
-                    "offer_quantity": 2
+                    "offer_quantity": 2,
                     }
-              }
+                
+            }
+
+
+
+test_cases = [
+    "",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "a",
+    "-",
+    "ABCa",
+    "AxA",
+    "ABCDEF",
+    "A",
+    "AA",
+    "AAA",
+    "AAAA",
+    "AAAAA",
+    "AAAAAA",
+    "AAAAAAA",
+    "AAAAAAAA",
+    "AAAAAAAAA",
+    "AAAAAAAAAA",
+    "EE",
+    "EEB",
+    "EEEB",
+    "EEEEBB",
+    "BEBEEE",
+    "A",
+    "AA",
+    "AAA",
+    "AAAA",
+    "AAAAA",
+    "AAAAAA",
+    "B",
+    "BB",
+    "BBB",
+    "BBBB",
+    "FF",
+    "FFF",
+    "FFFF",
+    "FFFFFF",
+    "FFFFFF",
+    "ABCDEFABCDEF",
+    "CDFFAECBDEAB",
+    "FFABCDECBAABCABBAAAEEAAFF",
+]
+
+
 
 def checkout(skus):
-    
-    if not input_validation(skus=skus):
+    if not input_validation(skus):
         return -1
-    
-    return calculate_basket_total(skus=skus)
-
-def calculate_basket_total(skus):
-    basket_total = 0
-    a_count = skus.count("A")
-    b_count = skus.count("B")
-    c_count = skus.count("C")
-    d_count = skus.count("D")
-    e_count = skus.count("E")
-    f_count = skus.count("F")
-
-    a_total_price = 0
-
-    offers_applied = []
-
-    if a_count >= ITEM_TABLE.get("A").get("offer_quantity") and a_count < ITEM_TABLE.get("A").get("offer_quantity_1"):
-        quotient, remainder = divmod(a_count, ITEM_TABLE.get("A").get("offer_quantity"))
-        a_total_price = (quotient * ITEM_TABLE.get("A").get("offer_price")) + (remainder * ITEM_TABLE.get("A").get("price"))
-        offers_applied.append("A") #Handle case of 3A for 130
-    elif a_count >= ITEM_TABLE.get("A").get("offer_quantity_1"): #Handle case of 5A for 200 offer
-        quotient, remainder = divmod(a_count, ITEM_TABLE.get("A").get("offer_quantity_1"))
-        if remainder >= ITEM_TABLE.get("A").get("offer_quantity"):
-            new_q, new_r = divmod(remainder, ITEM_TABLE.get("A").get("offer_quantity"))
-            a_total_price += (new_q * ITEM_TABLE.get("A").get("offer_price")) + (new_r * ITEM_TABLE.get("A").get("price"))
-            a_total_price += (quotient * ITEM_TABLE.get("A").get("offer_price_1")) #Handle case where user combines 3A and 5A offers 
-            offers_applied.append("A")
-            offers_applied.append("A_1")
-        else:
-            a_total_price += (quotient * ITEM_TABLE.get("A").get("offer_price_1")) + (remainder * ITEM_TABLE.get("A").get("price"))
-            offers_applied.append("A_1")
-    else:
-        a_total_price = ITEM_TABLE.get("A").get("price") * a_count
-
-    if b_count >= ITEM_TABLE.get("B").get("offer_quantity"):
-        quotient, remainder = divmod(b_count, ITEM_TABLE.get("B").get("offer_quantity"))
-        b_total_price = (quotient * ITEM_TABLE.get("B").get("offer_price")) + (remainder * ITEM_TABLE.get("B").get("price"))
-        offers_applied.append("B")
-    else:
-        b_total_price = ITEM_TABLE.get("B").get("price") * b_count
-
-    if e_count >= ITEM_TABLE.get("E").get("offer_quantity") and b_count:
-        e_offer_count = 0
-        if "B" in offers_applied:
-            b_total_price  = ITEM_TABLE.get("B").get("price") * b_count #Reset b price to remove any B offers
-        quotient, _ = divmod(e_count, ITEM_TABLE.get("E").get("offer_quantity"))
-        eligible_savings = quotient * ITEM_TABLE.get("B").get("price")
-        #print("eligible_savings", eligible_savings)
-        for i in range(b_count): 
-            if eligible_savings:
-                basket_total -= ITEM_TABLE.get("B").get("price")
-                eligible_savings -= ITEM_TABLE.get("B").get("price")
-                e_offer_count += 1
-        if e_offer_count < b_count:
-            basket_total += (e_offer_count * ITEM_TABLE.get("B").get("price"))
-            remaining_b = (b_count - e_offer_count)
-            if remaining_b >= ITEM_TABLE.get("B").get("offer_quantity"):
-                quotient_b, remainder_b = divmod(remaining_b, ITEM_TABLE.get("B").get("offer_quantity"))
-                b_total_price = (quotient_b * ITEM_TABLE.get("B").get("offer_price")) + (remainder_b * ITEM_TABLE.get("B").get("price"))
-                #print("b_here", b_total_price)
-            else:
-                b_total_price = (remaining_b * ITEM_TABLE.get("B").get("price"))
-    if f_count >= ITEM_TABLE.get("F").get("offer_quantity") + 1:
-        free_f = 0
-        quotient, remainder = divmod(f_count, ITEM_TABLE.get("F").get("offer_quantity") + 1)
-        for i in range(quotient):
-            free_f += 1
-        f_total_price = (f_count * ITEM_TABLE.get("F").get("price")) - (free_f * ITEM_TABLE.get("F").get("price"))
-    else:
-        f_total_price = (f_count * ITEM_TABLE.get("F").get("price"))        
-                
-        
-    c_total_price = ITEM_TABLE.get("C").get("price") * c_count
-    d_total_price = ITEM_TABLE.get("D").get("price") * d_count
-    e_total_price = ITEM_TABLE.get("E").get("price") * e_count
-
-    basket_total += (a_total_price + b_total_price + d_total_price + c_total_price + e_total_price + f_total_price)
-
-    return basket_total
-
-
-
-
-
-
-
-    """
-    if a_count >= ITEM_TABLE.get("A").get("offer_quantity"):
-        quotient, remainder = divmod(a_count, ITEM_TABLE.get("A").get("offer_quantity"))
-        a_total_price = (quotient * ITEM_TABLE.get("A").get("offer_price")) + (remainder * ITEM_TABLE.get("A").get("price"))
-        offers_applied.append("A")
-    else:
-        a_total_price = ITEM_TABLE.get("A").get("price") * a_count
-
-    if b_count >= ITEM_TABLE.get("B").get("offer_quantity"):
-        quotient, remainder = divmod(b_count, ITEM_TABLE.get("B").get("offer_quantity"))
-        b_total_price = (quotient * ITEM_TABLE.get("B").get("offer_price")) + (remainder * ITEM_TABLE.get("B").get("price"))
-    else:
-        b_total_price = ITEM_TABLE.get("B").get("price") * b_count
-
-    c_total_price = ITEM_TABLE.get("C").get("price") * c_count
-
-    d_total_price = ITEM_TABLE.get("D").get("price") * d_count
-
-    e_total_price = ITEM_TABLE.get("E").get("price") * e_count
-
-    return a_total_price + b_total_price + c_total_price + d_total_price + e_total_price
-    """
+    skus_count = {item: skus.count(item) for item in skus}
+    #print(skus_count)
+    offers_applied = {}
+    return calculate_basket_total(skus_count=skus_count, offers_applied=offers_applied)
 
 def input_validation(skus):
     
@@ -157,3 +113,33 @@ def input_validation(skus):
         if sku not in ITEM_TABLE:
             return False
     return True
+
+def calculate_basket_total(skus_count, offers_applied):
+    basket_total = 0
+    for sku in skus_count:
+        sku_total = apply_offer(offer_type=ITEM_TABLE.get(sku).get("offer_type"), item_id=sku, item_count = skus_count.get(sku), offers_applied=offers_applied, skus_count=skus_count)
+        basket_total += sku_total
+    return basket_total
+
+def apply_offer(offer_type, item_id, item_count, offers_applied, basket_total = 0, **kwargs):
+
+                                                                                                               
+    if item_id not in offers_applied:
+        if offer_type == "BOGO":
+            basket_total += apply_bogo(item_count=item_count, item_id=item_id, offers_applied=offers_applied)
+        elif offer_type == "BOGOO":
+            basket_total += apply_bogoo(item_count=item_count, kwargs=kwargs, item_id=item_id, offers_applied=offers_applied)
+        elif offer_type == "PRICE_REDUCT_MULTI":
+            basket_total += apply_price_reduct_multi(item_count=item_count, item_id=item_id, offers_applied=offers_applied)
+        elif offer_type == "PRICE_REDUCT_SINGLE":
+            basket_total += apply_price_reduct_single(item_id=item_id,item_count=item_count, offers_applied=offers_applied)
+        elif not offer_type:
+            basket_total += apply_normal_pricing(item_count=item_count, item_id=item_id)
+
+    return basket_total
+
+
+for test in test_cases:
+    print(test,">>>",checkout(test))
+
+#print(checkout("KK"))
---
+++ lib/solutions/CHK/offer_service.py
@@ -0,0 +1,83 @@
+from constants import ITEM_TABLE
+
+def apply_normal_pricing(item_count, item_id):
+    item_total_price = ITEM_TABLE.get(item_id).get("price") * item_count
+    return item_total_price
+    
+def apply_price_reduct_single(item_id, item_count, offers_applied):
+    if item_count >= ITEM_TABLE.get(item_id).get("offer_quantity"):
+        quotient, remainder = divmod(item_count, ITEM_TABLE.get(item_id).get("offer_quantity"))
+        item_total_price = (quotient * ITEM_TABLE.get(item_id).get("offer_price")) + (remainder * ITEM_TABLE.get(item_id).get("price"))
+        offers_applied[item_id] = item_total_price
+    else:
+        item_total_price = ITEM_TABLE.get(item_id).get("price") * item_count
+    return item_total_price
+    
+def apply_price_reduct_multi(item_count, item_id, offers_applied):
+    item_total_price = 0
+    if item_count >= ITEM_TABLE.get(item_id).get("offer_quantity") and item_count < ITEM_TABLE.get(item_id).get("offer_quantity_1"):
+        quotient, remainder = divmod(item_count, ITEM_TABLE.get(item_id).get("offer_quantity"))
+        item_total_price = (quotient * ITEM_TABLE.get(item_id).get("offer_price")) + (remainder * ITEM_TABLE.get(item_id).get("price"))
+        offers_applied[item_id] = item_total_price
+    elif item_count >= ITEM_TABLE.get(item_id).get("offer_quantity_1"):
+        quotient, remainder = divmod(item_count, ITEM_TABLE.get(item_id).get("offer_quantity_1"))
+        if remainder >= ITEM_TABLE.get(item_id).get("offer_quantity"):
+            new_q, new_r = divmod(remainder, ITEM_TABLE.get(item_id).get("offer_quantity"))
+            item_total_price += (new_q * ITEM_TABLE.get(item_id).get("offer_price")) + (new_r * ITEM_TABLE.get(item_id).get("price"))
+            item_total_price += (quotient * ITEM_TABLE.get(item_id).get("offer_price_1")) #Handle case where user combines 3A and 5A offers 
+            offers_applied[item_id] = (new_q * ITEM_TABLE.get(item_id).get("offer_price")) + (new_r * ITEM_TABLE.get(item_id).get("price"))
+            offers_applied[f"{item_id}_1"] = (quotient * ITEM_TABLE.get(item_id).get("offer_price_1"))
+        else:
+            item_total_price += (quotient * ITEM_TABLE.get(item_id).get("offer_price_1")) + (remainder * ITEM_TABLE.get(item_id).get("price"))
+            offers_applied[f"{item_id}_1"] = item_total_price
+    else:
+        item_total_price = ITEM_TABLE.get(item_id).get("price") * item_count
+    return item_total_price
+
+def apply_bogo(item_count, item_id, offers_applied):
+    free_items = 0
+    if item_count >= ITEM_TABLE.get(item_id).get("offer_quantity") + 1:
+        quotient, remainder = divmod(item_count, ITEM_TABLE.get(item_id).get("offer_quantity") + 1)
+        for i in range(quotient):
+            free_items += 1
+        item_total_price = (item_count * ITEM_TABLE.get(item_id).get("price")) - (free_items * ITEM_TABLE.get(item_id).get("price"))
+        offers_applied[item_id] = item_total_price
+    else:
+        item_total_price = (item_count * ITEM_TABLE.get(item_id).get("price"))
+    return item_total_price
+
+def apply_bogoo(item_count, kwargs, item_id, offers_applied):
+    free_items = 0
+    offer_count = 0
+    item_total_price = 0
+    bogoo_item_id = ITEM_TABLE.get(item_id).get("bogoo_item")
+    bogoo_item_count = kwargs.get("skus_count").get(bogoo_item_id)
+            
+    if item_count >= ITEM_TABLE.get(item_id).get("offer_quantity") and bogoo_item_count:
+        #Reset bogoo item price
+        if bogoo_item_id in offers_applied:
+            item_total_price -= offers_applied.get(bogoo_item_id)
+   
+        # calculate free bogoo items
+        eligible_free_items, _ = divmod(item_count, ITEM_TABLE.get(item_id).get("offer_quantity"))
+        for i in range(bogoo_item_count):
+            if eligible_free_items:
+                item_total_price += ITEM_TABLE.get(bogoo_item_id).get("price")
+                free_items += 1
+                eligible_free_items -= 1 
+        item_total_price += (item_count * ITEM_TABLE.get(item_id).get("price")) - ((free_items) * ITEM_TABLE.get(bogoo_item_id).get("price"))
+        offers_applied[item_id] = item_total_price
+        offers_applied[bogoo_item_id] = item_total_price
+
+        #Recalculate bogoo item's original offer if there are remaining bogoo items that qualify
+        if free_items < bogoo_item_count:
+            remaining_bogoo_items = (bogoo_item_count - free_items)
+            if remaining_bogoo_items >= ITEM_TABLE.get(bogoo_item_id).get("offer_quantity"):
+                quotient_bogoo, remainder_bogoo = divmod(remaining_bogoo_items, ITEM_TABLE.get(bogoo_item_id).get("offer_quantity"))
+                item_total_price += (quotient_bogoo * ITEM_TABLE.get(bogoo_item_id).get("offer_price")) + (remainder_bogoo * ITEM_TABLE.get(bogoo_item_id).get("price"))
+                offers_applied[bogoo_item_id] = item_total_price
+            else:
+                item_total_price += (remaining_bogoo_items * ITEM_TABLE.get(bogoo_item_id).get("price"))
+    else:
+        item_total_price = ITEM_TABLE.get(item_id).get("price") * item_count
+    return item_total_price
