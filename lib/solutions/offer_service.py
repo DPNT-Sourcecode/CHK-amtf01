91b7a14ceef4d2f723314a619b102a9d362ccfec
sbx_user1051 <sbx_user1051@169.254.57.229>
2023-11-09 00:20:42 +0000

    Thu Nov 09 00:20:42 UTC 2023

--- lib/solutions/offer_service.py
+++ lib/solutions/offer_service.py
@@ -46,12 +46,12 @@ def apply_bogo(item_count, item_id, offers_applied):
         item_total_price = (item_count * ITEM_TABLE.get(item_id).get("price"))
     return item_total_price
 
-def apply_bogoo(item_count, kwargs, item_id, offers_applied):
+def apply_bogoo(item_count, skus_count, item_id, offers_applied):
     free_items = 0
     offer_count = 0
     item_total_price = 0
     bogoo_item_id = ITEM_TABLE.get(item_id).get("bogoo_item")
-    bogoo_item_count = kwargs.get("skus_count").get(bogoo_item_id)
+    bogoo_item_count = skus_count.get(bogoo_item_id)
             
     if item_count >= ITEM_TABLE.get(item_id).get("offer_quantity") and bogoo_item_count:
         #Reset bogoo item price
@@ -81,3 +81,46 @@ def apply_bogoo(item_count, kwargs, item_id, offers_applied):
     else:
         item_total_price = ITEM_TABLE.get(item_id).get("price") * item_count
     return item_total_price
+
+def apply_group_buy(group_buy_items, item_id, offers_applied):
+
+    group_buy_count = len(group_buy_items)
+    item_count = group_buy_items.count(item_id)
+    remainder_items = []
+    item_total_price = 0
+    if group_buy_count >= ITEM_TABLE.get(item_id).get("offer_quantity"):
+        if group_buy_count % ITEM_TABLE.get(item_id).get("offer_quantity") == 0:
+            quotient, _ = divmod(group_buy_count, ITEM_TABLE.get(item_id).get("offer_quantity"))
+            item_total_price = (quotient * ITEM_TABLE.get(item_id).get("offer_price"))
+            for item in set(group_buy_items): #prehaps change the check for group buy items to boolean
+                offers_applied[item] = item_total_price
+        else: #Maybe for loop here to dynamically generate the while loop (from group buy item with lowest price to highest price)
+            sorted_group_buy_keys = sorted((key for key, value in ITEM_TABLE.items() if value.get("offer_type") == "GROUP_BUY"), key=lambda key: ITEM_TABLE[key]["price"])
+            for key in sorted_group_buy_keys:
+                while key in group_buy_items:
+                    if group_buy_count % ITEM_TABLE.get(item_id).get("offer_quantity") != 0:
+                        group_buy_items.remove(key)
+                        remainder_items.append(key)
+                        group_buy_count = len(group_buy_items)
+                    else:
+                        break
+
+            #calculate group buy items
+
+            quotient, _ = divmod(len(group_buy_items), ITEM_TABLE.get(item_id).get("offer_quantity"))
+            item_total_price += quotient * ITEM_TABLE.get(item_id).get("offer_price")
+            for item in set(group_buy_items): #prehaps change the check for group buy items to boolean
+                offers_applied[item] = item_total_price
+
+            #calculate remainder items TODO: test out the notiion that remainder items do bot need to be calculated here. Since they will never be more than 3 remiander items, they would be calculated individually in the else block below.
+            for item in remainder_items:
+                item_total_price += ITEM_TABLE.get(item).get("price")
+                offers_applied[item] = offers_applied.get(item, 0) + ITEM_TABLE.get(item).get("price")
+
+                
+    else:
+        item_total_price += ITEM_TABLE.get(item_id).get("price") * item_count
+        offers_applied[item_id] = offers_applied.get(item_id, 0) + (ITEM_TABLE.get(item_id).get("price") * item_count)
+
+    return item_total_price
+
